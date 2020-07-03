213e2638dcef44530c6199b1712ef5f973d1c294
Supragya Shrestha <supragyashrestha02@gmail.com>
2020-07-03 14:25:07 +0530

    Snake Initial game

---
+++ Snake/sketch.js
@@ -0,0 +1,48 @@
+var s;
+
+function setup() {
+    createCanvas(600,600);
+    s = new Snake();
+    frameRate(7);
+    pick_food_location();
+}
+
+function pick_food_location() {
+    var col = floor(width/size);
+    var row = floor(height/size);
+    food = createVector(floor(random(col)) , floor(random(row)));
+    food.mult(size);
+}
+
+function mousePressed() {
+    s.total++;
+}
+  
+function draw() {
+    background(51);
+    if(s.eat(food)){
+        pick_food_location();
+    }
+    s.death();
+    s.update();
+    s.show();
+
+    fill(255,0,100);
+    rect(food.x,food.y,size,size);
+}
+
+function keyPressed() {
+    if(keyCode === UP_ARROW){
+        s.dir(0,-1);
+        //console.log("up");
+    }else if(keyCode === DOWN_ARROW){
+        s.dir(0,1);
+        //console.log("down");
+    }else if(keyCode === LEFT_ARROW){
+        s.dir(-1,0);
+        //console.log("left");
+    }else if(keyCode === RIGHT_ARROW){
+        s.dir(1,0);
+        //console.log("right");
+    }
+}
---
+++ Snake/snake.js
@@ -0,0 +1,61 @@
+var size = 12;
+
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
+
+    this.death = function() {
+        for( var i = 0; i < this.tail.length; i++){
+            var pos = this.tail[i];
+            var d = dist(this.x , this.y , pos.x , pos.y);
+            if(d<1){
+                console.log(this.total);
+                this.total = 0;
+                this.tail = [];
+            }
+        }
+    }
+
+    this.eat = function(pos){
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        if(d<1){
+            this.total++;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    this.dir = function(x,y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+    this.update = function() {
+        if(this.total === this.tail.length){
+            for( var i = 0; i < this.tail.length - 1; i++){
+                this.tail[i] = this.tail[i+1];
+            }
+        }
+        this.tail[this.total - 1] = createVector(this.x,this.y);
+
+        this.x = this.x + this.xspeed*size;
+        this.y = this.y + this.yspeed*size;
+
+        this.x = constrain(this.x, 0, width-size);
+        this.y = constrain(this.y, 0, height-size);
+    }
+
+    this.show = function() {
+        fill(255);
+        for( var i = 0; i < this.tail.length; i++){
+            rect(this.tail[i].x, this.tail[i].y , size, size);
+        }
+        rect(this.x, this.y, size , size);
+    }
+}
